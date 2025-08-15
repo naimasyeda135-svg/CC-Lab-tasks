fb0bde0cd80b178065f14b6f27d777662788f9cb
naimasyeda135-svg <naimasyeda135@gmail.com>
2025-08-16 00:27:18 +0600

    Lab 1

---
+++ Lab Task 1/multiplication/multiplication.s
@@ -0,0 +1,36 @@
+	.file	"multiplication.c"
+	.intel_syntax noprefix
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "%d/n\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	push	rbp
+	.seh_pushreg	rbp
+	mov	rbp, rsp
+	.seh_setframe	rbp, 0
+	sub	rsp, 48
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	mov	DWORD PTR -4[rbp], 117
+	mov	DWORD PTR -8[rbp], 2
+	mov	eax, DWORD PTR -4[rbp]
+	imul	eax, DWORD PTR -8[rbp]
+	mov	DWORD PTR -12[rbp], eax
+	mov	eax, DWORD PTR -12[rbp]
+	lea	rcx, .LC0[rip]
+	mov	edx, eax
+	call	printf
+	mov	eax, 0
+	add	rsp, 48
+	pop	rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (GNU) 15.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
